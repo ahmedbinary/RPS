5098b524809ce50db2f33fb0887a18e5b85373cb
ahmedAhmedHamed <102419703+StanP16@users.noreply.github.com>
2024-01-21 13:41:56 +0200

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,44 +1,57 @@
 import random
 
-choices = ["rock","paper","scissors"]
+choices = ["rock", "paper", "scissors"]
+
+
+class Choices:
+    ROCK = "rock"
+    SCISSORS = "scissors"
+    PAPER = "paper"
+
+
+def print_player_choices(player_choice, computer_choice):
+    print("computer_choice: " + computer_choice)
+    print("player_choice : " + player_choice)
+
+
+def get_computer_choice():
+    return random.choice(choices)
+
+
 while True:
-    computer = random.choice(choices)
-    player = None
-    while player not in choices:
+    computer_choice = get_computer_choice()
+    player_choice = None
+    while player_choice not in choices:
 
-        player = input("rock , paper or scissors?: ").lower()
-    if player == computer:
-        print("computer: "+ computer)
-        print("player : "+ player)
+        player_choice = input("rock , paper or scissors?: ").lower()
+    if player_choice == computer_choice:
+        print("computer_choice: "+ computer_choice)
+        print("player_choice : "+ player_choice)
         print("Tie!")
-    elif player == "rock":
-        if computer =="paper":
-            print("computer: "+ computer)
-            print("player : "+ player)
+    elif player_choice == Choices.ROCK:
+        if computer_choice =="paper":
+            print("computer_choice: " + computer_choice)
+            print("player_choice : " + player_choice)
             print("You Lose Bozo!")
-        elif computer == "scissors":
-            print("computer: "+ computer)
-            print("player : "+ player)
-            print("You deafeted a computer GJ!!")
-    elif player =="paper":
-        if computer == "scissors":
-            print("computer: "+ computer)
-            print("player : "+ player)
+        elif computer_choice == "scissors":
+            print("computer_choice: " + computer_choice)
+            print("player_choice : " + player_choice)
+            print("You deafeted a computer_choice GJ!!")
+    elif player_choice == "paper":
+        if computer_choice == "scissors":
+            print_player_choices(player_choice, computer_choice)
             print("You Lose Bozo!")
-        elif computer == "rock":
-            print("computer: "+ computer)
-            print("player : "+ player)
-            print("You deafeted a computer GJ!!")
+        elif computer_choice == "rock":
+            print_player_choices(player_choice, computer_choice)
+            print("You deafeted a computer_choice GJ!!")
 
-    elif player =="scissors":
-        if computer == "rock":
-            print("computer: "+ computer)
-            print("player : "+ player)
+    elif player_choice =="scissors":
+        if computer_choice == "rock":
+            print_player_choices(player_choice, computer_choice)
             print("You lose Bozo!")
-        elif computer == "paper":
-            print("computer: "+ computer)
-            print("player : "+ player)
-            print("You deafeted a computer GJ!!")
+        elif computer_choice == "paper":
+            print_player_choices(player_choice, computer_choice)
+            print("You deafeted a computer_choice GJ!!")
     playAgain = input("Press 'y' to play again or press any key to get out of here ").lower()
     if playAgain !="y":
         break
